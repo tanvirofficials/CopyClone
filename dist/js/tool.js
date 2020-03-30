6ae78c0d93b355ef7d57c4b5213b834620e2336a
Tanvir Islam <tanvirofficials3216@gmail.com>
2020-03-30 18:13:47 +0600

    aded custom button text feature

--- dist/js/tool.js
+++ dist/js/tool.js
@@ -1161,6 +1161,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
 
 
 
@@ -2212,26 +2214,36 @@ var render = function() {
           on: { click: _vm.fire }
         },
         [
-          _c(
-            "svg",
-            {
-              staticClass: "fill-current",
-              attrs: {
-                xmlns: "http://www.w3.org/2000/svg",
-                viewBox: "0 0 24 24",
-                width: "22",
-                height: "22"
-              }
-            },
-            [
-              _c("path", {
-                attrs: {
-                  d:
-                    "M17 7h2.25c.97 0 1.75.78 1.75 1.75v10.5c0 .97-.78 1.75-1.75 1.75H8.75C7.78 21 7 20.22 7 19.25V17H4.75C3.78 17 3 16.22 3 15.25V4.75C3 3.78 3.78 3 4.75 3h10.5c.97 0 1.75.78 1.75 1.75V7zm-2 0V5H5v10h2V8.75C7 7.78 7.78 7 8.75 7H15zM9 9v10h10V9H9z"
-                }
-              })
-            ]
-          )
+          (this.panel.fields[0].button_text
+          ? true
+          : false)
+            ? _c("span", [_vm._v(_vm._s(this.panel.fields[0].button_text))])
+            : _vm._e(),
+          _vm._v(" "),
+          (this.panel.fields[0].button_text
+          ? false
+          : true)
+            ? _c(
+                "svg",
+                {
+                  staticClass: "fill-current",
+                  attrs: {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    viewBox: "0 0 24 24",
+                    width: "22",
+                    height: "22"
+                  }
+                },
+                [
+                  _c("path", {
+                    attrs: {
+                      d:
+                        "M17 7h2.25c.97 0 1.75.78 1.75 1.75v10.5c0 .97-.78 1.75-1.75 1.75H8.75C7.78 21 7 20.22 7 19.25V17H4.75C3.78 17 3 16.22 3 15.25V4.75C3 3.78 3.78 3 4.75 3h10.5c.97 0 1.75.78 1.75 1.75V7zm-2 0V5H5v10h2V8.75C7 7.78 7.78 7 8.75 7H15zM9 9v10h10V9H9z"
+                    }
+                  })
+                ]
+              )
+            : _vm._e()
         ]
       ),
       _vm._v(" "),
